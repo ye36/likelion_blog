73e1c48a94968dbc0d12ce2fd321f357080f611b
Your Name <you@example.com>
2019-03-07 22:16:43 +0900

    ddd

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 import blog.views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('blog/', blog.views.home, name='home'),
     path('blog/<int:blog_id>', blog.views.detail, name='detail'),
     path('blog/new/', blog.views.new, name='new'),
